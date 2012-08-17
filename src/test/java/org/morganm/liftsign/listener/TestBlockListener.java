4b81d1059d89b82e87cf06fa01e4f003d03d317f
Mark Morgan <morganm.qc@gmail.com>
2012-08-16 22:28:45 -0700

    update copyright/license header

--- src/test/java/org/morganm/liftsign/listener/TestBlockListener.java
+++ src/test/java/org/morganm/liftsign/listener/TestBlockListener.java
@@ -4,11 +4,6 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
@@ -24,6 +19,11 @@
  * notice, this list of conditions and the following disclaimer
  * in the documentation and/or other materials provided with the
  * distribution.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
  * Contributors:
  *     Mark Morgan - initial API and implementation
